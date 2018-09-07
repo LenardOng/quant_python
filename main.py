208e585d06ddc192ea8975922f880449d619c122
Lenard DESKTOP <lenardong@gmail.com>
2018-09-07 18:15:42 +0800

    Example linear/polynomial line of best fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn import datasets, linear_model
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 import os
 from matplotlib import pyplot as plt
@@ -16,7 +16,7 @@ file_loc = os.path.join(data_loc, file_name)
 df = pd.read_csv(file_loc)
 
 # Print file head, data format with years, level 1/2, industry and value
-print(df.head())
+#print(df.head())
 
 #Rename value to total
 d = {'value':'Total'}
@@ -27,9 +27,36 @@ nett_year = df.groupby(['year'], as_index=False)['value'].sum().rename(columns=d
 x_year = nett_year['year']
 y_nett = nett_year['Total']
 #Initialise plot
+plt.figure(figsize=(12, 9))
 ax = plt.subplot(111)
-#Plotting function with nicer defaults
-plot.plot_nice_lineplot(x_year, y_nett, title='Yearly manufacturing output, millions/SGD', ax=ax)
 
+
+#Convert to array
+x_year = np.expand_dims(np.array(x_year), axis=1)
+y_nett = np.expand_dims(np.array(y_nett), axis=1)
+
+#plot_nice_lineplot is a plotting function with nicer defaults
+
+#Plot original data
+plot.plot_nice_lineplot(x_year, y_nett, title='Yearly manufacturing output, millions/SGD', ax=ax, line_label='Original')
+
+#Plot unregularised linear fit
+lr = LinearRegression()
+lr.fit(x_year, y_nett)
+y_fitted = lr.predict(x_year)
+plot.plot_nice_lineplot(x_year, y_fitted, ax=ax, col_index=6, line_label='Linear Fit', linestyle='--')
+
+#Plot unregularised polynomial fit
+poly_lr = LinearRegression()
+
+x_norm = x_year - x_year[0]
+x_poly = np.concatenate((x_norm, x_norm**2), axis = 1)
+
+poly_lr.fit(x_poly, y_nett)
+y_fitted = poly_lr.predict(x_poly)   
+print(y_fitted.shape)
+plot.plot_nice_lineplot(x_year, y_fitted, ax=ax, col_index=14, line_label='Square Fit', linestyle='--')
+
+#Show graph
 plt.show()
 
--- util/plot.py
+++ util/plot.py
@@ -1,7 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Tableau colours for easier replication
+# Tableau colours for easier replication, colour values from
+# https://public.tableau.com/profile/chris.gerrard#!/vizhome/TableauColors/ColorPaletteswithRGBValues
 tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
              (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),    
              (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),    
@@ -12,14 +13,17 @@ tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
 gray = np.repeat(199/255, 3)  
              
              
-def plot_nice_lineplot(x, y, title=None, y_label=None, x_label=None, ax=None, col_index=0):
+def plot_nice_lineplot(x, y, title=None, y_label=None, x_label=None, ax=None, col_index=0, line_label=None, linestyle='-'):
     if not ax:
         ax = plt.subplot(111)
+
     ax.spines["top"].set_visible(False)    
     ax.spines["bottom"].set_visible(False)    
     ax.spines["right"].set_visible(False)    
     ax.spines["left"].set_visible(False)    
-    plt.plot(x, y, lw=2.5, color=[x/255.0 for x in tableau20[col_index]])
+   
+    
+    plt.plot(x, y, lw=2.5, color=[x/255.0 for x in tableau20[col_index]], linestyle=linestyle)
     ax.grid(color=gray, linestyle='--', linewidth=0.8)
     #for y in range(10, 91, 10):    
     #    plt.plot(range(1968, 2012), [y] * len(range(1968, 2012)), "--", lw=0.5, color="black", alpha=0.3)    
@@ -31,6 +35,10 @@ def plot_nice_lineplot(x, y, title=None, y_label=None, x_label=None, ax=None, co
         plt.ylabel(y_label)
     if x_label:
         plt.xlabel(x_label)
+        
+    if line_label:
+        y_pos = y[-1]
+        plt.text(2016.5, y_pos-1000, line_label, fontsize=14, color=[x/255.0 for x in tableau20[col_index]])  
     return 0
 
 
