4fe887b0bd4aaa6c9e175cff1b37faf6668bb9b7
Lenard DESKTOP <lenardong@gmail.com>
2018-09-07 17:25:32 +0800

    Basic plotting from data csv file

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 import numpy as np
 import pandas as pd
-import scikit-learn as sl
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+import os
+from matplotlib import pyplot as plt
+from util import plot
 
+#Dataset from https://data.gov.sg/dataset/total-output-manufacturing-annual
+#Download to data/ and rename to total-output.csv
+data_loc = 'data'
+file_name = 'total-output.csv'
 
+#Read in data
+file_loc = os.path.join(data_loc, file_name)
+df = pd.read_csv(file_loc)
 
+# Print file head, data format with years, level 1/2, industry and value
+print(df.head())
 
+d = {'year':'Year', 'value':'Total / million SGD'}
+# Pandas group by year then sum
+nett_year = df.groupby(['year'], as_index=False)['value'].sum().rename(columns=d)
+
+#Gathering x and y data
+x_year = nett_year['Year']
+y_nett = nett_year['Total / million SGD']
+#Initialise plot
+ax = plt.subplot(111)
+#Plotting function with nicer defaults
+plot.plot_nice_lineplot(x_year, y_nett, title='Yearly manufacturing output, millions/SGD', ax=ax)
+
+plt.show()
 
---
+++ util/plot.py
@@ -0,0 +1,38 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Tableau colours for easier replication
+tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
+             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),    
+             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),    
+             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),    
+             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]    
+
+
+gray = np.repeat(199/255, 3)  
+             
+             
+def plot_nice_lineplot(x, y, title=None, y_label=None, x_label=None, ax=None, col_index=0):
+    if not ax:
+        ax = plt.subplot(111)
+    ax.spines["top"].set_visible(False)    
+    ax.spines["bottom"].set_visible(False)    
+    ax.spines["right"].set_visible(False)    
+    ax.spines["left"].set_visible(False)    
+    plt.plot(x, y, lw=2.5, color=[x/255.0 for x in tableau20[col_index]])
+    ax.grid(color=gray, linestyle='--', linewidth=0.8)
+    #for y in range(10, 91, 10):    
+    #    plt.plot(range(1968, 2012), [y] * len(range(1968, 2012)), "--", lw=0.5, color="black", alpha=0.3)    
+    plt.xticks(fontsize=12)    
+    plt.yticks(fontsize=12) 
+    if title:
+        plt.title(title, fontsize=16)
+    if y_label:
+        plt.ylabel(y_label)
+    if x_label:
+        plt.xlabel(x_label)
+    return 0
+
+
+
+
