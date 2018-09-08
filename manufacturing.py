47c10a157f82e00a4e0e59b14bdbfedaffe22a19
Lenard DESKTOP <lenardong@gmail.com>
2018-09-09 01:35:03 +0800

    heatmap plot example up

--- manufacturing.py
+++ manufacturing.py
@@ -7,6 +7,11 @@ from matplotlib import pyplot as plt
 from util import plot
 #plot_nice_lineplot is a plotting function with nicer defaults
 
+#Number of level_2 industries to plot
+n_ind = 21
+#Limit colormesh
+vmin_ = -75
+vmax_ = 75
 
 #----- DATASET LOADING -----#
 
@@ -30,20 +35,54 @@ d = {'value':'Total'}
 # Pandas group by year then sum
 nett_year = df.groupby(['year'], as_index=False)['value'].sum().rename(columns=d)
 
+# Select n largest indices only (make plot clearer)
+largest_n_ind = (df[df['year']==2016]).nlargest(n_ind, 'value')
+
+# Pandas group by year and level_2 industry
+industries = largest_n_ind['level_2'].unique()
+x_year = nett_year['year']
+print('Total number of level_2 industries is {}'.format((df['level_2'].unique()).shape[0]))
+print('Plotting the {} largest industries'.format(n_ind))
+
+
+
 #Gathering x and y data
 x_year = nett_year['year']
 y_nett = nett_year['Total']
-#Initialise plot
-plt.figure(figsize=(12, 9))
-ax = plt.subplot(111)
 
 #Convert to array
 x_year = np.expand_dims(np.array(x_year), axis=1)
 y_nett = np.expand_dims(np.array(y_nett), axis=1)
 
+#Initialise plot
+fig = plt.figure(figsize=(15, 9))
+ax = plt.subplot(111)
 
+
+y_store = []
+# Plot individual level_2 industry value
+for i, ind in enumerate(industries):
+    temp = np.array((df.loc[:, ['value']])[df['level_2']==ind])
+    
+    differences = np.convolve(temp[:, 0], [1, -1], mode='valid')
+    y_ind=np.divide(differences, temp[0:temp.shape[0]-1, 0])*100
+    y_store.append(y_ind)
+
+
+y_plot = np.array(y_store)
+y_plot = np.clip(y_plot, -50, 50)
+
+print('industry shape {}, x year shape {}, dat shape {}'.format(industries.shape[0], x_year[:, 0].shape[0], y_plot.shape))
+
+im, cbar = plot.heatmap(y_plot, industries, x_year[1:, 0], ax=ax,
+                   cmap="RdBu", cbarlabel="Growth in %/year", vmin=vmin_, vmax=vmax_)
+#texts = plot.annotate_heatmap(im, valfmt="{x:.1f} t")
+fig.tight_layout()
+
+
+"""
 #Plot original data
-plot.plot_nice_lineplot(x_year, y_nett, title='Yearly manufacturing output, millions/SGD', ax=ax, line_label='Original')
+plot.plot_nice_lineplot(x_year, y_nett, title='Yearly manufacturing output, millions/SGD', ax=ax, line_label='Original')    
 
 #----- LINEAR MODEL FITTING -----#
 
@@ -53,10 +92,7 @@ lr.fit(x_year, y_nett)
 y_fitted = lr.predict(x_year)
 plot.plot_nice_lineplot(x_year, y_fitted, ax=ax, col_index=6, line_label='Linear Fit', linestyle='--')
 
-
-
 #----- SECOND ORDER MODEL FITTING -----#
-
 
 #Plot unregularised polynomial fit
 poly_lr = LinearRegression()
@@ -66,6 +102,8 @@ poly_lr.fit(x_poly, y_nett)
 y_fitted = poly_lr.predict(x_poly)   
 plot.plot_nice_lineplot(x_year, y_fitted, ax=ax, col_index=14, line_label='Square Fit', linestyle='--')
 
+
+"""
+
 #Show graph
 plt.show()
-
